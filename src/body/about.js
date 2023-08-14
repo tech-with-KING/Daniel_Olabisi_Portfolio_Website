cd467092142f55c03ecf3d7581beae29fbe9a926
Kingsley Okpo <kngsley2018@gmail.com>
2023-08-14 00:04:36 -0400

    this is the final look of the website , I would be pushing to production now

--- src/body/about.js
+++ src/body/about.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import imgr from "./sc/mo_prev_ui.png"
 import "./about.css"
 const About=(props)=>{
     const {toggle}=props
@@ -14,19 +13,27 @@ const About=(props)=>{
     <section id="know_me">
         <div className="know_me container">
             <div className="col-left">
-            <div className="col-img" style={{backgroundImage:'url(/img/IMG_20220317_162022.jpg)'}}>
+            <div className="col-img" style={{}}>
 	         <div className="imgd"></div>
                 </div>
             </div>
 
-            <div className="col-right">
+            <div id='class-action' className="col-right">
                 <h1 className="section-title">About <span>Me</span></h1>
-                <h2>Front End developer</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                    Veritatis sit tenetur doloribus totam reiciendis incidunt assumenda,
-                     fugit iure a optio quas quaerat expedita. 
-                    Reiciendis voluptates sit dolor ipsam fugiat! Blanditiis!</p>
-                    <a href="#" className="cta">Download resume</a>
+                <h2 >Data Analyst</h2>
+                <p  className='class-action'>
+                    Daniel Olabisi is a graduate of the Federal University of Technology with a degree in Telecommunication Engineering. His journey in the field of engineering provided him with the
+                     opportunity to intern with an Internet Service Provider, where he discovered his passion for problem-solving and providing solutions to clients.
+Currently, Daniel is dedicated to advancing his skills in the field of data analytics through comprehensive soft and 
+technical training with ALX Africa. His strong desire for effective problem-solving and data-driven decision-making fuels his commitment to learning and growth.
+  
+    Aspiring to dominate the data space, he is driven to proffer innovative solutions to brands 
+    in the industry and aims to keep climbing high. Daniel's goal is to make a significant impact in the
+     data analytics field, contributing to data-driven strategies that empower businesses to achieve their goals.
+His achievements encompass a constant pursuit of growth and purpose, as well as a transformative impact on 
+the lives of people around him. He looks forward to connecting with potential opportunities and exploring how he can 
+contribute his passion and skills to an organization's success.
+                </p>
             </div>
         </div>
     </section>
--- src/body_new/glassy.js
+++ src/body_new/glassy.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './glassy.css'; // Import your CSS file for styling
-import img from './IMG_6608.JPG'
-
+import img from './WhatsApp Image 2023-08-13 at 5.24.05 PM.jpeg'
+import { Link } from 'react-router-dom';
 const GlassyProfile = () => {
-    const text = `
-As a passionate software engineer and mathematician, I thrive on the exhilarating intersection of logic and creativity. With a solid foundation in mathematics, I approach software development as an elegant puzzle that demands both analytical thinking and innovative solutito learn new languages, frameworks, and paradigms to stay at the forefront of innovatlly to the world of technology.
+    const text = `Daniel Olabisi is a graduate of the Federal University of Technology with a degree in Telecommunication Engineering. His journey in the field of engineering provided him with the opportunity to intern with an Internet Service Provider, where he discovered his passion for problem-solving and providing solutions to clients.
+    Currently, Daniel is dedicated to advancing his skills in the field of data analytics through comprehensive soft and technical training with ALX Africa. 
+  
 `
   return (
     <div className="glassy-profile">
       <div className="text-side">
-        <h1>BIO</h1>
-        <p>{text}</p>
+        <h2 className='glassytitle'>BIO :</h2>
+        <h1 className='glassy-header'>"Passionate about Data Analytics | Solving Problems, Driving Growth"</h1>
+        <p>{text}
+        <a className='personally' href='#class-action'> Continue Reading</a>
+        </p>
       </div>
       <div className="image-side">
         <img src={img} alt="Profile" />
